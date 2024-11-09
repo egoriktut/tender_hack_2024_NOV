1da517b7c7c1a7a90edfb5b2d84a5da1fda67e5b
HAKI2 <salawathaki@gmail.com>
2024-11-09 03:58:09 +0300

    fix types

--- app/ai_model.py
+++ app/ai_model.py
@@ -8,13 +8,13 @@ class ModelInference:
         # No model loading needed for mock
         pass
 
-    def validate_content(self, page_data: KSAttributes, validate_params: List[ValidationOption]) -> Dict[str, Any]:
+    def validate_content(self, page_data: KSAttributes, validate_params: List[ValidationOption]) -> Dict[ValidationOption, bool]:
         # Simulate a delay to mimic real model processing time
         time.sleep(2)
         # TODO: call model to compare
         import random
         result = {
-            str(id_param): bool(random.randint(0, 1))
+            ValidationOption(id_param): bool(random.randint(0, 1))
             for id_param in validate_params
         }
         return result
--- app/tasks.py
+++ app/tasks.py
@@ -3,12 +3,12 @@ from app.scraper import fetch_and_parse
 from app.ai_model import ModelInference
 from app.config import settings
 from typing import Dict, Any, List
-from app.schemas.api import ValidationOption, AnalyzeUrlRequest
+from app.schemas.api import ValidationOption, AnalyzeUrlRequest, Result, Optional
 
 model_inference = ModelInference(settings.MODEL_PATH)
 
 @celery_app.task
-def start_analysis_task(url: str, validate_params: List[ValidationOption]) -> Dict[str, Any]:
+def start_analysis_task(url: str, validate_params: List[ValidationOption]) -> Dict:
 
     """
     This task fetches and parses content from a URL, then runs it through an AI model for analysis.
@@ -17,8 +17,8 @@ def start_analysis_task(url: str, validate_params: List[ValidationOption]) -> Di
     :return: A dictionary containing the URL and the analysis result.
     """
     page_data = fetch_and_parse(url)  # Assuming `fetch_and_parse` returns a dict with 'content' key
-    if page_data is not None:
+    if page_data is None:
         raise Exception()
     analysis_result = model_inference.validate_content(page_data, validate_params)
 
-    return {"url": url, "analysis": analysis_result}
+    return Result(url=url, analysis=analysis_result).dict()
