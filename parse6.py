3c83491ee764cf824dddb6262b85f43d2e713989
dmitry <dimon.sudak2015@gmail.com>
2024-11-10 13:45:27 +0300

    brrr15

--- parse6.py
+++ parse6.py
@@ -62,8 +62,7 @@ for table in tables:
                             print("wanna new prev id [", list(df.iloc(i))[0], "]")
                             # print(df.iloc(i))
                             prev_item_id = int(list(df.iloc[i])[0]) - 1
-                            for i in range(len(all_doc_specs), prev_item_id + 1):
-                                all_doc_specs.append(["" for i in range(table_wid)])
+                            all_doc_specs.append(["" for i in range(table_wid)])
 
 
                         except:
